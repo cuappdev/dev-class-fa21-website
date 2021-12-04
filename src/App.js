d435fee0dd39ee9127424fdff0c0de9f2941fdb5
Kidus Zegeye <kmz25@cornell.edu>
2021-12-04 16:39:30 -0500

    resize jins

--- src/App.js
+++ src/App.js
@@ -5,14 +5,9 @@ import { Modal, Button, Form } from "react-bootstrap";
 import React, { useState } from 'react';
 
 import './App.css';
-import { data } from 'jquery';
 
 
 function App() {
-  const [show, setShow] = useState(false);
-
-  const handleShow = (id) => setShow(!show)
-  
   return (
     <>
     <div className="App" >
@@ -31,7 +26,7 @@ function App() {
       <div className="container px-4">
         <div className="row gx-5">
           <div className="col">
-            <div className="card" onClick={handleShow("kidus")} >
+            <div className="card">
               <img src={jin} className="card-img-top" alt="jin" />
               <div className="card-body">
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -39,15 +34,16 @@ function App() {
             </div>
           </div>
           <div className="col">
-            <div className="card" >
+            <div className="card"  >
               <img src={jin} className="card-img-top" alt="jin" />
               <div className="card-body">
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
               </div>
-            </div>
+          </div>
           </div>
           <div className="col">
-            <div className="card" >
+            <div className="card"  >
+
               <img src={jin} className="card-img-top" alt="jin" />
               <div className="card-body">
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -57,17 +53,147 @@ function App() {
           
         </div>
       </div>
-    </div>
-    <Modal show={show}>
-        <Modal.Header closeButton  onClick={handleShow}>
-          <Modal.Title>Login Form</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <div className="data">
-            
+
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4 text-light">IOS</h1>
+        </div>
+      </div>
+      <div className="container px-4">
+        <div className="row gx-5">
+          <div className="col">
+            <div className="card"  >
+              <img src={jin} className="card-img-top" alt="jin" />
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
           </div>
-        </Modal.Body>
-      </Modal>
+          <div className="col">
+            <div className="card"  >
+              <img src={jin} className="card-img-top" alt="jin" />
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+          </div>
+          </div>
+          <div className="col">
+            <div className="card"  >
+
+              <img src={jin} className="card-img-top" alt="jin" />
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4 text-light">Marketing</h1>
+        </div>
+      </div>
+
+      <div className="container px-4 col-md-7">
+        <div className="row gx-5">
+          <div className="col">
+            <div className="card"  >
+              <img src={jin} className="card-img-top" alt="jin" />
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card"  >
+              <img src={jin} className="card-img-top" alt="jin" />
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+          </div>
+          </div>
+          
+          
+        </div>
+      </div>
+
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4 text-light">Design</h1>
+        </div>
+      </div>
+
+
+      <div className="container px-4">
+        <div className="row gx-5">
+          <div className="col">
+            <div className="card"  >
+              <img src={jin} className="card-img-top" alt="jin" />
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card"  >
+              <img src={jin} className="card-img-top" alt="jin" />
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card"  >
+              <img src={jin} className="card-img-top" alt="jin" />
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+          </div>
+          </div>
+          <div className="col">
+            <div className="card"  >
+
+              <img src={jin} className="card-img-top" alt="jin" />
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+
+
+
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4 text-light">Backend</h1>
+        </div>
+      </div>
+
+      <div className="container px-4 col-md-7">
+        <div className="row gx-5">
+          <div className="col">
+            <div className="card"  >
+              <img src={jin} className="card-img-top" alt="jin" />
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card"  >
+              <img src={jin} className="card-img-top" alt="jin" />
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+          </div>
+          </div>
+          
+        </div>
+      </div>
+    </div>
     </>
   );
 }
